38c5e6f894f1f2dd044141a7f5ed7204407473a4
Harshit Chauhan <m4070k@gmail.com>
2025-06-12 15:36:14 +0530

    gRPC client server

--- go.mod
+++ go.mod
@@ -12,5 +12,11 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
+	google.golang.org/grpc v1.73.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -21,7 +21,7 @@ func TestLog(t *testing.T) {
 		"truncate":                          testTruncate,
 	} {
 		t.Run(scenario, func(t *testing.T) {
-			dir, err := os.MkdirTemp("", "store-test")
+			dir, err := os.MkdirTemp(os.TempDir(), "store-test")
 			require.NoError(t, err)
 			defer os.RemoveAll(dir)
 			c := Config{}
@@ -50,7 +50,8 @@ func testAppendRead(t *testing.T, log *Log) {
 func testOutOfRangeErr(t *testing.T, log *Log) {
 	read, err := log.Read(1)
 	require.Nil(t, read)
-	require.Error(t, err)
+	apiErr := err.(api.ErrOffsetOutOfRange)
+	require.Equal(t, uint64(1), apiErr.Offset)
 }
 
 func testInitExisting(t *testing.T, o *Log) {
